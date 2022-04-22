883b33885926040c9f3b2cc21ded7585528c296c
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-23 00:16:36 +0100

    Removal

--- src/client/Core/ClientInitialisation.client.lua
+++
@@ -1,7 +0,0 @@
---ClientInitialisation
-
---Modules
-local SharedModule = require(game.ReplicatedStorage.Common.Modules.SharedInitialisation)
-
-print("Hello World! This message was sent from the Client.")
-SharedModule.ClientModuleInitialisation()
--- src/server/Core/Scripts/ServerInitialisation.server.lua
+++
@@ -1,7 +0,0 @@
---ServerInitialisation
-
---Modules
-local SharedModule = require(game.ReplicatedStorage.Common.Modules.SharedInitialisation)
-
-print("Hello World! This message was sent from the Server.")
-SharedModule.ServerModuleInitialisation()
