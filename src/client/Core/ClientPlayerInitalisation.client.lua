716825b26131d59561737a5748c5eef57e5b64e0
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-22 14:16:38 +0100

    Added ToolInitalisation Script on Activation within the Character.

--- src/client/Core/ClientPlayerInitalisation.client.lua
+++ src/client/Core/ClientPlayerInitalisation.client.lua
@@ -1,1 +1,28 @@
 --Client Player Initalisation
+
+--Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--PreRequisites
+local CurrentPlayer = game.Players.LocalPlayer
+local CurrentCharacter = game.Workspace:WaitForChild(CurrentPlayer.Name)
+
+--Events
+local ToolActivatedEvent = ReplicatedStorage.Common.Events.ToolActivatedEvent
+
+--RunTime
+CurrentCharacter.ChildAdded:Connect(function(AddedChild)
+    if AddedChild:IsA("Tool") then
+        AddedChild.Activated:Connect(function()
+            task.spawn(function()
+                local Success, ErrorData = pcall(function()
+                    ToolActivatedEvent:FireServer(AddedChild.Name)
+                end)
+
+                if not Success then
+                    print(ErrorData)
+                end
+            end)
+        end)
+    end
+end)
