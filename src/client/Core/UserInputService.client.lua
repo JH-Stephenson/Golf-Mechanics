445800e6f8d6c12f7096948f795d61c4cf6c9a14
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-22 13:06:12 +0100

    Added Other Console Access to UIS.

--- src/client/Core/UserInputService.client.lua
+++ src/client/Core/UserInputService.client.lua
@@ -9,7 +9,7 @@ local UserInputRegistered = false
 --UserInputServiceRunTime
 UserInputService.InputBegan:Connect(function(GameInput, GameProcessedEvent)
     if UserInputRegistered == true then
-        if GameInput.UserInputType == Enum.UserInputType.MouseButton1 then
+        if GameInput.UserInputType == Enum.UserInputType.MouseButton1 or Enum.UserInputType.Touch or Enum.UserInputType.Gamepad1 then
             print("Print UserInputRegistered is true.")
         end
     end
