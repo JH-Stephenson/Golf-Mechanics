81cff8c68ee3e660caa5dc1a3964472f00402a8a
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-23 10:32:15 +0100

    Added RaycastingModule on New RoJo Codebase.

---
+++ src/server/Core/Modules/Raycasting/RaycastingModule.lua
@@ -0,0 +1,30 @@
+local RaycastingModule = {}
+
+function RaycastingModule.FindPlayersGround(Player)
+    --WorkspacePreRequisites
+    local WorkspacePlayer = Player.Character
+    local WorkspacePlayerRoot = WorkspacePlayer:FindFirstChild("HumanoidRootPart")
+
+    --RaycastingParameters
+    local RaycastOrigin = WorkspacePlayerRoot.Position
+    local RaycastDirection = Vector3.new(0, -20, 0)
+    
+    local RaycastParameters = RaycastParams.new()
+    RaycastParameters.FilterType = Enum.RaycastFilterType.Blacklist
+    RaycastParameters.FilterDescendantsInstances = {WorkspacePlayer}
+    RaycastParameters.IgnoreWater = false
+
+    --RaycastCreation
+    local Raycast = workspace:Raycast(RaycastOrigin, RaycastDirection, RaycastParameters)
+
+    if Raycast then
+        print("Player's Raycast Hit Object: ", Raycast.Instance:GetFullName())
+        print("Player's Raycast Hit Object at Position: ", Raycast.Position)
+        print("Player's Raycast Hit an Object with the Material: ", Raycast.Material.Name)
+        print("Player's Raycast Surface Normal at Position: ", Raycast.Normal)
+
+        return Raycast
+    end
+end
+
+return RaycastingModule
