778a171827517c224c4965695ec7831c8e1369e3
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-23 14:56:05 +0100

    Added Events, Enabling UserInputRegister and on PowerBarEnabled.

--- src/client/UI/UserInputRecgonition.client.lua
+++ src/client/UI/UserInputRecgonition.client.lua
@@ -7,11 +7,26 @@ local UserInputService = game:GetService("UserInputService")
 --Variables
 local UserInputRegister = false
 
+--Events
+local GolfDriverClient = ReplicatedStorage.Common.Events.Tools.GolfDriver.GolfDriverClientSend
+local GolfDriverServer = ReplicatedStorage.Common.Events.Tools.GolfDriver.GolfDriverClientRecieve
+
+--UI
+local PowerBarUI = game.Players.LocalPlayer.PlayerGui.GolfPowerBar
+
+GolfDriverClient.OnClientEvent:Connect(function()
+    UserInputRegister = true
+end)
+
 --RunTime
 UserInputService.InputBegan:Connect(function(GameInput, GameProcessedEvent)
     if UserInputRegister == true then
         if GameInput.UserInputType == Enum.UserInputType.MouseButton1 or Enum.UserInputType.Touch or Enum.UserInputType.Gamepad1 then
             print("Print UserInputRegistered is true.")
+
+            if PowerBarUI.Enabled == true then
+                print("YEET")
+            end
         end
     end
 end)
