a6300536c8663393fb4a40473293d399e61cb290
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-24 12:32:20 +0100

    Added GolfTee and GolfBall Modules to the ToolActivatedEvent.

--- src/server/Core/Scripts/Tools/ToolServer.server.lua
+++ src/server/Core/Scripts/Tools/ToolServer.server.lua
@@ -3,6 +3,10 @@
 --Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+--Modules
+local GBCM = require(game.ServerScriptService.Server.Core.Modules.ObjectCreation.GolfBallCreator)
+local GTCM = require(game.ServerScriptService.Server.Core.Modules.ObjectCreation.GolfTeeCreator)
+
 --ToolEvents
 local ToolActivatedEvent = ReplicatedStorage.Common.Events.Tools.ToolActivatedEvent
 
@@ -10,7 +14,11 @@ local ToolActivatedEvent = ReplicatedStorage.Common.Events.Tools.ToolActivatedEv
 ToolActivatedEvent.OnServerEvent:Connect(function(ServicePlayer, ToolName)
     task.spawn(function()
         local Success, ErrorData = pcall(function()
-            print(ToolName)
+            if ToolName == ("Golf Ball") then
+                GBCM.CreateGolfBall(ServicePlayer, game.Workspace:FindFirstChild(ServicePlayer.UserId.."'s Golf Ball"), game.Workspace:FindFirstChild(ServicePlayer.UserId.."'s Golf Tee"))
+            elseif ToolName == ("Tee") then
+                GTCM.CreateGolfTee(ServicePlayer, game.Workspace:FindFirstChild(ServicePlayer.UserId.."'s Golf Tee"))
+            end
         end)
 
         if not Success then
