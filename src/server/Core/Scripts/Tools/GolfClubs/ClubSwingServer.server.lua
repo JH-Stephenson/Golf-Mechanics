a498ad9045fc035baeb457cac252bf3d393a5527
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-24 20:28:22 +0100

    Created Initials for SwingServer.

---
+++ src/server/Core/Scripts/Tools/GolfClubs/ClubSwingServer.server.lua
@@ -0,0 +1,9 @@
+--ClubSwingServer
+
+--Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--GolfClubEvents
+local DriverHit = ReplicatedStorage.Common.Events.Tools.GolfClubs.GolfDriver.HitGolfBall
+
+--RunTime
