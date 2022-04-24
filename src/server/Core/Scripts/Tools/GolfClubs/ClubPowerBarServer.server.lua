07384ed02f47da7916257dffac4743a8499ff8d8
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-24 19:09:14 +0100

    CreatedPowerBarServer

---
+++ src/server/Core/Scripts/Tools/GolfClubs/ClubPowerBarServer.server.lua
@@ -0,0 +1,30 @@
+--ClubPowerBarServer
+
+--Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--Events
+local ClubClientActivate = ReplicatedStorage.Common.Events.Tools.GolfClubs.ClubClientActivate
+local ClubPowerBarSubmit = ReplicatedStorage.Common.Events.Tools.GolfClubs.ClubPowerBarSubmit
+
+ClubPowerBarSubmit.OnServerEvent:Connect(function(ServicePlayer, ClubType, PowerRate)
+    task.spawn(function()
+        local Success, ErrorData = pcall(function()
+            local PlayerGui = ServicePlayer.PlayerGui
+            local PowerBarUI = PlayerGui:WaitForChild("GolfPowerBar")
+            PowerBarUI.Enabled = false
+
+            local WorkspaceCharacter = game.Workspace:FindFirstChild(ServicePlayer.Name)
+            local WorkspaceClub = WorkspaceCharacter:FindFirstChild(ClubType)
+            local ClubHead = WorkspaceClub:FindFirstChild(ClubType.."Head")
+
+            ClubHead:SetAttribute("GolfBallTouch", "True")
+            wait(3)
+            ClubHead:SetAttribute("GolfBallTouch", "False")
+        end)
+
+        if not Success then
+            print(ErrorData)
+        end
+    end)
+end)
