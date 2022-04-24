c15cf6b999137830fb5259210f50c4e9f661c840
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-24 16:57:01 +0100

    Created ClubInitalisation

--- src/server/Core/Modules/ToolModules/GolfClubs/GolfClubModule.lua
+++ src/server/Core/Modules/ToolModules/GolfClubs/GolfClubModule.lua
@@ -3,16 +3,23 @@
 --Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
---Events
-
-
 --Module
 local GolfClubModule = {}
+
+--Events
+local ClubClientActivate = ReplicatedStorage.Common.Events.Tools.GolfClubs.ClubClientActivate
+local ClubPowerBarSubmit = ReplicatedStorage.Common.Events.Tools.GolfClubs.ClubPowerBarSubmit
 
 function GolfClubModule.InitaliseClub(ServicePlayer, GolfClubName)
     task.spawn(function()
         local Success, ErrorData = pcall(function()
+            local PlayerUI = ServicePlayer.PlayerGui
+            local PowerBarUI = PlayerUI:FindFirstChild("GolfPowerBar")
             
+            wait(3)
+
+            ClubClientActivate:FireClient(ServicePlayer, "Driver")
+            PowerBarUI.Enabled = true
         end)
 
         if not Success then
