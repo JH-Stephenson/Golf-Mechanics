c4aabf2822cdcccda5c2800e868713ec9b2fa1b9
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-24 18:14:49 +0100

    Created Initials for ClientSide PowerBar

--- src/client/UI/UserInputService.client.lua
+++ src/client/UI/UserInputService.client.lua
@@ -3,11 +3,14 @@
 --Services
 local PlayersService = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local UserInputService = game:GetService("UserInputService")
 
 --PlayerVariables
 local ServicePlayer = PlayersService.LocalPlayer
 local PlayerGui = ServicePlayer.PlayerGui
+
 local PowerBarUI = PlayerGui:FindFirstChild("GolfPowerBar")
+local PowerBar = PowerBarUI.OuterFrame.InnerFrame.PowerSlider
 
 --Events
 local ClubClientActivate = ReplicatedStorage.Common.Events.Tools.GolfClubs.ClubClientActivate
@@ -25,15 +28,98 @@ local Wedge = false
 local Wood = false
 
 ClubClientActivate.OnClientEvent:Connect(function(ServicePlayer, ClubType)
-    if ClubType == ("Driver") then
-        Driver = true
-    elseif ClubType == ("Iron") then
-        Iron = true
-    elseif ClubType == ("Putter") then
-        Putter = true
-    elseif ClubType == ("Wedge") then
-        Wedge = true
-    elseif ClubType == ("Wood") then
-        Wood = true
-    end
+    task.spawn(function()
+        local Success, ErrorData = pcall(function()
+            if ClubType == ("Driver") then
+                Driver = true
+            elseif ClubType == ("Iron") then
+                Iron = true
+            elseif ClubType == ("Putter") then
+                Putter = true
+            elseif ClubType == ("Wedge") then
+                Wedge = true
+            elseif ClubType == ("Wood") then
+                Wood = true
+            end
+        end)
+
+        if not Success then
+            print(ErrorData)
+        end
+    end)
+end)
+
+UserInputService.InputBegan:Connect(function(GameInput, GameProcessedEvent)
+    task.spawn(function()
+        local Success, ErrorData = pcall(function()
+            if GameInput.UserInputType == Enum.UserInputType.MouseButton1 or Enum.UserInputType.Touch or Enum.UserInputType.Gamepad1 then
+                if PowerBarUI.Enabled == true then
+                    UserInputRegister = true
+
+                    while UserInputRegister == true do
+                        for PowerCounter = 1, 320, 1 do
+                            PowerBar.Position = UDim2.new(0, PowerCounter, 0, 0)
+    
+                            if UserInputRegister == false then
+                                break
+                            end
+    
+                            UserPowerRate = PowerCounter
+                            task.wait()
+    
+                            if PowerCounter == 320 then
+                                for PowerCounter = 320, 1, -1 do
+                                    PowerBar.Position = UDim2.new(0, PowerCounter, 0, 0)
+
+                                    if UserInputRegister == false then
+                                        break
+                                    end
+
+                                    UserPowerRate = PowerCounter
+                                    task.wait()
+                                end
+                            end
+                        end
+                        task.wait()
+                    end
+                end
+            end
+        end)
+
+        if not Success then
+            print(ErrorData)
+        end
+    end)
+end)
+
+UserInputService.InputEnded:Connect(function(GameInput, GameProcessedEvent)
+    task.spawn(function()
+        local Success, ErrorData = pcall(function()
+            if Driver == true then
+                UserInputRegister = false
+                Driver = false
+
+            elseif Iron == true then
+                UserInputRegister = false
+                Iron = false
+
+            elseif Putter == true then 
+                UserInputRegister = false
+                Putter = false
+
+            elseif Wedge == true then
+                UserInputRegister = false
+                Wedge = false
+
+            elseif Wood == true then
+                UserInputRegister = false
+                Wood = false
+                
+            end
+        end)
+
+        if not Success then
+            print(ErrorData)
+        end
+    end)
 end)
