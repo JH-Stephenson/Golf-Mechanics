5e78e3f05498008f2a0473be4047609dfebb3413
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-24 21:35:30 +0100

    Created InitialStarting Point for SwingServer.

--- src/server/Core/Scripts/Tools/GolfClubs/ClubSwingServer.server.lua
+++ src/server/Core/Scripts/Tools/GolfClubs/ClubSwingServer.server.lua
@@ -7,3 +7,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local DriverHit = ReplicatedStorage.Common.Events.Tools.GolfClubs.GolfDriver.HitGolfBall
 
 --RunTime
+DriverHit.OnServerEvent:Connect(function(ServicePlayer)
+    
+end)
