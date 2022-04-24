826e4d02d90da645277b7d1b97476bde212b8c63
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-24 12:27:08 +0100

    Created Base for ToolActivated on Server requires Implimentation of ToolModules.

--- src/server/Core/Scripts/Tools/ToolServer.server.lua
+++ src/server/Core/Scripts/Tools/ToolServer.server.lua
@@ -4,9 +4,17 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 --ToolEvents
-local ToolActivatedEvent = ReplicatedStorage.Common.Events.ToolActivatedEvent
+local ToolActivatedEvent = ReplicatedStorage.Common.Events.Tools.ToolActivatedEvent
 
 --RunTime
 ToolActivatedEvent.OnServerEvent:Connect(function(ServicePlayer, ToolName)
-    
+    task.spawn(function()
+        local Success, ErrorData = pcall(function()
+            print(ToolName)
+        end)
+
+        if not Success then
+            print(ErrorData)
+        end
+    end)
 end)
