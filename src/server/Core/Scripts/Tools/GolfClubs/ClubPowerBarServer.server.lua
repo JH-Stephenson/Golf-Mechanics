9e9bcde6cb768de3f52ff761d7babaf0145dc5b5
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-24 20:28:11 +0100

    Added New Attributes to Prevent Failed Runs.

--- src/server/Core/Scripts/Tools/GolfClubs/ClubPowerBarServer.server.lua
+++ src/server/Core/Scripts/Tools/GolfClubs/ClubPowerBarServer.server.lua
@@ -10,20 +10,23 @@ local ClubPowerBarSubmit = ReplicatedStorage.Common.Events.Tools.GolfClubs.ClubP
 ClubPowerBarSubmit.OnServerEvent:Connect(function(ServicePlayer, ClubType, PowerRate)
     task.spawn(function()
         local Success, ErrorData = pcall(function()
-            local PlayerGui = ServicePlayer.PlayerGui
-
-            local PowerBarUI = PlayerGui:WaitForChild("GolfPowerBar")
-            PowerBarUI.Enabled = false
-            ServicePlayer:SetAttribute("CurrentlySwinging", "True")
-
             local WorkspaceCharacter = game.Workspace:FindFirstChild(ServicePlayer.Name)
+            local PlayerGui = ServicePlayer.PlayerGui
+            local PowerBarUI = PlayerGui:WaitForChild("GolfPowerBar")
+
             local WorkspaceClub = WorkspaceCharacter:FindFirstChild(ClubType)
             local ClubHead = WorkspaceClub:FindFirstChild("Head")
 
+            PowerBarUI.Enabled = false
+            ServicePlayer:SetAttribute("CurrentPowerRate", PowerRate)
+            ServicePlayer:SetAttribute("CurrentlySwinging", "True")
             ClubHead:SetAttribute("GolfBallTouch", "True")
+
             wait(3)
+
             ClubHead:SetAttribute("GolfBallTouch", "False")
             ServicePlayer:SetAttribute("CurrentlySwinging", "False")
+            ServicePlayer:SetAttribute("CurrentPowerRate", 0)
         end)
 
         if not Success then
