202ecff53fbd515d8c10ae237b98550f4bf8f280
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-25 14:58:20 +0100

    Created Initial Module

---
+++ src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
@@ -0,0 +1,11 @@
+--CFrameCalculator
+
+--Module
+local CFrameCalculator = {}
+
+function CFrameCalculator.CalculatePart1(HumanoidRootPart, SwingStrength)
+    
+end
+
+--EndModule
+return CFrameCalculator
