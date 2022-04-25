04f1ea9e92413265c85ff5d9f44cdfe7eb5578db
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-04-25 18:01:37 +0100

    Removed Failed CFrame Creation.

--- src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
+++ src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
@@ -3,37 +3,22 @@
 --Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+--Variables
+local WindSpeed = ReplicatedStorage.Common.Components.Objects.WindSpeed.WindSpeed
+
 --Module
 local CFrameCalculator = {}
 
-function CFrameCalculator.CalculatePart2(HumanoidRootPart, SwingStrength, StartPart)
-    --PLEASE JAMES CREATE THE WINDSPEED
-    local WindSpeed = 5
+function CFrameCalculator.CalculatePart2()
 
-    local QuarterWindSpeed = (-SwingStrength / 4)
-    local HalfWindSpeed = (-SwingStrength / 2)
-
-    return StartPart.CFrame * CFrame.new(QuarterWindSpeed - WindSpeed, HalfWindSpeed + 5, HalfWindSpeed - -5)
 end
 
 function CFrameCalculator.CalculatePart3(HumanoidRootPart, SwingStrength, StartPart)
-    --PLEASE JAMES CREATE THE WINDSPEED
-    local WindSpeed = 5
 
-    local QuarterWindSpeed = (-SwingStrength / 4)
-    local HalfWindSpeed = (-SwingStrength / 2)
-
-    return StartPart.CFrame * CFrame.new(HalfWindSpeed -WindSpeed, HalfWindSpeed + 7, HalfWindSpeed - 10)
 end
 
-function CFrameCalculator.CalculatePart4(HumanoidRootPart, SwingStrength, StartPart)
-    --PLEASE JAMES CREATE THE WINDSPEED
-    local WindSpeed = 5
+function CFrameCalculator.CalculatePart4()
 
-    local QuarterWindSpeed = (-SwingStrength / 4)
-    local HalfWindSpeed = (-SwingStrength / 2)
-
-    return StartPart.CFrame * CFrame.new(HalfWindSpeed, 10, HalfWindSpeed - -15)
 end
 
 --EndModule
