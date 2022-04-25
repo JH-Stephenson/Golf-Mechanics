741a73cc126214a865bf61245b9eadb60131999e
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-25 15:51:00 +0100

    Added FindPartGround to Raycast Module

--- src/server/Core/Modules/Raycasting/RaycastingModule.lua
+++ src/server/Core/Modules/Raycasting/RaycastingModule.lua
@@ -27,4 +27,27 @@ function RaycastingModule.FindPlayersGround(ServicePlayer)
     end
 end
 
+function RaycastingModule.FindPartGround(PartName)
+    --RaycastingParameters
+    local RaycastOrigin = PartName.Position
+    local RaycastDirection = Vector3.new(0, -20, 0)
+    
+    local RaycastParameters = RaycastParams.new()
+    RaycastParameters.FilterType = Enum.RaycastFilterType.Blacklist
+    RaycastParameters.FilterDescendantsInstances = {PartName}
+    RaycastParameters.IgnoreWater = false
+
+    --RaycastCreation
+    local Raycast = workspace:Raycast(RaycastOrigin, RaycastDirection, RaycastParameters)
+
+    if Raycast then
+        print("Player's Raycast Hit Object: ", Raycast.Instance:GetFullName())
+        print("Player's Raycast Hit Object at Position: ", Raycast.Position)
+        print("Player's Raycast Hit an Object with the Material: ", Raycast.Material.Name)
+        print("Player's Raycast Surface Normal at Position: ", Raycast.Normal)
+
+        return Raycast
+    end
+end
+
 return RaycastingModule
