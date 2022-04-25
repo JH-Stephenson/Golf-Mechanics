8ceef211e57a175069dc12b4191f09ee4414b0d3
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-25 14:47:41 +0100

    Created Initial Function + Start Part for Bezier

--- src/server/Core/Modules/GolfClubs/SwingModule.lua
+++ src/server/Core/Modules/GolfClubs/SwingModule.lua
@@ -2,13 +2,38 @@
 
 --Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+--RequiredModules
+local Raycasting = require(game.ServerScriptService.Server.Core.Modules.Raycasting.RaycastingModule)
+local BezierModule = require(game.ServerScriptService.Server.Core.Modules.BezierCurves.BezierModule)
 
 --Module
 local SwingModule = {}
 
 --RunTime
-function SwingModule.HitBall(GolfSwingDistance)
-    
+function SwingModule.HitBall(ServicePlayer, GolfSwingDistance)
+    task.spawn(function()
+        local Success, ErrorData = pcall(function()
+            local GolfBall = game.Workspace:FindFirstChild(ServicePlayer.UserId.."'s Golf Ball")
+            local GroundRaycast = Raycasting.FindPlayersGround(ServicePlayer)
+
+            if GolfBall then
+                if GroundRaycast then
+                    local StartPart = Instance.new("Attachment")
+                    StartPart.Transparency = 1
+                    StartPart.Name = (ServicePlayer.UserId.."'s Start Bezier Part")
+                    StartPart.Anchored = true
+                    StartPart.CanCollide = false
+                    StartPart.CFrame = GolfBall.CFrame
+                end
+            end
+        end)
+
+        if not Success then
+            print(ErrorData)
+        end
+    end)
 end
 
 
