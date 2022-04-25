e8badc62e4b363a7c1417b0720b458ff7b10bc9e
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-25 15:13:10 +0100

    Creared Ubutuak Calculator

--- src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
+++ src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
@@ -1,10 +1,19 @@
 --CFrameCalculator
 
+--Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 --Module
 local CFrameCalculator = {}
 
-function CFrameCalculator.CalculatePart1(HumanoidRootPart, SwingStrength)
-    
+function CFrameCalculator.CalculatePart1(HumanoidRootPart, SwingStrength, StartPart)
+    --PLEASE JAMES CREATE THE WINDSPEED
+    local WindSpeed = 0
+
+    local QuarterWindSpeed = (-SwingStrength / 4)
+    local HalfWindSpeed = (-SwingStrength / 2)
+
+    return StartPart.CFrame * CFrame.new(QuarterWindSpeed - WindSpeed, HalfWindSpeed + 5, HalfWindSpeed - -5)
 end
 
 --EndModule
