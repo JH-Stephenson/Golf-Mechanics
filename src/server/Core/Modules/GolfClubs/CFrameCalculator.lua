c72f449f8fb8fe997ce2e5e999cb7cea0e5d28af
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-25 15:45:09 +0100

    Added Calculation for BezierPart4

--- src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
+++ src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
@@ -6,7 +6,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 --Module
 local CFrameCalculator = {}
 
-function CFrameCalculator.CalculatePart1(HumanoidRootPart, SwingStrength, StartPart)
+function CFrameCalculator.CalculatePart2(HumanoidRootPart, SwingStrength, StartPart)
     --PLEASE JAMES CREATE THE WINDSPEED
     local WindSpeed = 5
 
@@ -16,7 +16,7 @@ function CFrameCalculator.CalculatePart1(HumanoidRootPart, SwingStrength, StartP
     return StartPart.CFrame * CFrame.new(QuarterWindSpeed - WindSpeed, HalfWindSpeed + 5, HalfWindSpeed - -5)
 end
 
-function CFrameCalculator.CalculatePart2(HumanoidRootPart, SwingStrength, StartPart)
+function CFrameCalculator.CalculatePart3(HumanoidRootPart, SwingStrength, StartPart)
     --PLEASE JAMES CREATE THE WINDSPEED
     local WindSpeed = 5
 
@@ -24,6 +24,16 @@ function CFrameCalculator.CalculatePart2(HumanoidRootPart, SwingStrength, StartP
     local HalfWindSpeed = (-SwingStrength / 2)
 
     return StartPart.CFrame * CFrame.new(HalfWindSpeed -WindSpeed, HalfWindSpeed + 7, HalfWindSpeed - 10)
+end
+
+function CFrameCalculator.CalculatePart4(HumanoidRootPart, SwingStrength, StartPart)
+    --PLEASE JAMES CREATE THE WINDSPEED
+    local WindSpeed = 5
+
+    local QuarterWindSpeed = (-SwingStrength / 4)
+    local HalfWindSpeed = (-SwingStrength / 2)
+
+    return StartPart.CFrame * CFrame.new(HalfWindSpeed, 10, HalfWindSpeed - -15)
 end
 
 --EndModule
