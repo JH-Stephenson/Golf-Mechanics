f1730bd4c691a8ab8d15347efd878b6555a238a4
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-25 15:42:46 +0100

    Added Calculation for BezierPoint3

--- src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
+++ src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
@@ -16,5 +16,15 @@ function CFrameCalculator.CalculatePart1(HumanoidRootPart, SwingStrength, StartP
     return StartPart.CFrame * CFrame.new(QuarterWindSpeed - WindSpeed, HalfWindSpeed + 5, HalfWindSpeed - -5)
 end
 
+function CFrameCalculator.CalculatePart2(HumanoidRootPart, SwingStrength, StartPart)
+    --PLEASE JAMES CREATE THE WINDSPEED
+    local WindSpeed = 5
+
+    local QuarterWindSpeed = (-SwingStrength / 4)
+    local HalfWindSpeed = (-SwingStrength / 2)
+
+    return StartPart.CFrame * CFrame.new(HalfWindSpeed -WindSpeed, HalfWindSpeed + 7, HalfWindSpeed - 10)
+end
+
 --EndModule
 return CFrameCalculator
