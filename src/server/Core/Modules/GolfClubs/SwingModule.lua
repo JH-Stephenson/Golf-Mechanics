65ac47245bc9bf3c19541eca1ac4fb549f8d4519
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-25 14:14:19 +0100

    Created Initials for the SwingModule to Hit the GolfBall.

---
+++ src/server/Core/Modules/GolfClubs/SwingModule.lua
@@ -0,0 +1,16 @@
+--SwingModule
+
+--Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--Module
+local SwingModule = {}
+
+--RunTime
+function SwingModule.HitBall(GolfSwingDistance)
+    
+end
+
+
+--EndModule
+return SwingModule
