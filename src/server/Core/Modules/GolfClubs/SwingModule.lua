fc284b1caa9d185e5cfcefdb1987a064ac56edfb
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-25 15:17:52 +0100

    Added WorkspaceCharacter and CharacterRoot + BezierPoint1 (Start) BezierPoint2 (Mid1)

--- src/server/Core/Modules/GolfClubs/SwingModule.lua
+++ src/server/Core/Modules/GolfClubs/SwingModule.lua
@@ -16,17 +16,27 @@ local SwingModule = {}
 function SwingModule.HitBall(ServicePlayer, GolfSwingDistance)
     task.spawn(function()
         local Success, ErrorData = pcall(function()
+            local WorkspaceCharacter = game.Workspace:FindFirstChild(ServicePlayer.Name)
+            local CharacterRoot = WorkspaceCharacter:FindFirstChild("HumanoidRootPart")
+
             local GolfBall = game.Workspace:FindFirstChild(ServicePlayer.UserId.."'s Golf Ball")
             local GroundRaycast = Raycasting.FindPlayersGround(ServicePlayer)
 
             if GolfBall then
                 if GroundRaycast then
-                    local StartPart = Instance.new("Attachment")
-                    StartPart.Transparency = 1
-                    StartPart.Name = (ServicePlayer.UserId.."'s Start Bezier Part")
-                    StartPart.Anchored = true
-                    StartPart.CanCollide = false
-                    StartPart.CFrame = GolfBall.CFrame
+                    local BezierPoint1 = Instance.new("Part")
+                    BezierPoint1.Transparency = 1
+                    BezierPoint1.Name = (ServicePlayer.UserId.."'s Bezier Point 1")
+                    BezierPoint1.Anchored = true
+                    BezierPoint1.CanCollide = false
+                    BezierPoint1.CFrame = GolfBall.CFrame
+
+                    local BezierPoint2 = Instance.new("Part")
+                    BezierPoint2.Transparency = 1
+                    BezierPoint2.Name = (ServicePlayer.UserId.."'s Bezier Point 2")
+                    BezierPoint2.Anchored = true
+                    BezierPoint2.CanCollide = false
+                    BezierPoint2.CFrame = GolfSwingCFrames.CalculatePart1(CharacterRoot, GolfSwingDistance, BezierPoint1)
                 end
             end
         end)
