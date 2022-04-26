82986a30203e0c943174066a0b8a1ec9fa1cfc29
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-26 09:47:49 +0100

    Finished CFrame Calculator

--- src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
+++ src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
@@ -3,6 +3,9 @@
 --Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+--Modules
+local Raycasting = require(game.ServerScriptService.Server.Core.Modules.Raycasting.RaycastingModule)
+
 --Variables
 local WindSpeed = ReplicatedStorage.Common.Components.Objects.WindSpeed.WindSpeed.Value
 
@@ -29,7 +32,7 @@ function CFrameCalculator.CalculatePart3(BezierPoint1, SwingStrength)
     return BezierPoint1.CFrame * CFrame.new(HalfWindSpeed - WindSpeed, HalfWindSpeed + 7, HalfWindSpeed -10)
 end
 
-function CFrameCalculator.CalculatePart4(BezierPoint1, SwingStrength)
+function CFrameCalculator.CalculatePart4CFrame(BezierPoint1, SwingStrength)
     local HalfSwingStrength = -SwingStrength / 2
     local QuarterSwingStrength = -SwingStrength / 4
 
@@ -39,11 +42,16 @@ function CFrameCalculator.CalculatePart4(BezierPoint1, SwingStrength)
     return BezierPoint1.CFrame * CFrame.new(HalfWindSpeed - WindSpeed, 10, HalfWindSpeed - -15)
 end
 
+function CFrameCalculator.CalculatePart4Position(BezierPoint1, BezierPoint4, SwingStrength)
+    local Raycast = Raycasting.FindPartGround(BezierPoint4)
+    return Raycast.Position + Vector3.new(0, (BezierPoint4.Size.Y / 2), 0)
+end
+
 --EndModule
 return CFrameCalculator
 
 --[[
-    Comments for Building Relevant Calculations
+Comments for Building Relevant Calculations
 return CFrameCalculator
 
 WillTakeThePartToTheLeft
@@ -52,4 +60,7 @@ PartName.CFrame = PartName.CFrame * CFrame.new(0,0,1) -- Change this based on wh
 MidPart1.CFrame = StartPart.CFrame * CFrame.new((-SwingDistance / 4) -WindSpeedValue, 5 + (WindSpeedValue / 2), -5 - (WindSpeedValue / 2))
 MidPart2.CFrame = StartPart.CFrame * CFrame.new((-SwingDistance / 2) -WindSpeedValue, 7 + (WindSpeedValue / 2), -10 - (WindSpeedValue / 2))
 EndPart.CFrame = StartPart.CFrame * CFrame.new(-SwingDistance / 2, 10, -15 - (WindSpeedValue / 2))
+
+local BezierPoint4Raycast = Raycasting.FindPartGround(BezierPoint4)
+BezierPoint4.Position = BezierPoint4Raycast.Position + Vector3.new(0, (BezierPoint4.Size.Y / 2), 0)
 ]]
