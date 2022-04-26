5502054cf176bb9c52e61dfc93eb43d8102186ce
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-04-26 09:29:55 +0100

    Finished CFrame Calculator

--- src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
+++ src/server/Core/Modules/GolfClubs/CFrameCalculator.lua
@@ -16,23 +16,40 @@ function CFrameCalculator.CalculatePart2(BezierPoint1, SwingStrength)
     local HalfWindSpeed = -WindSpeed / 2
     local QuarterWindSpeed = -WindSpeed / 4
 
-    return CFrame * CFrame.new(QuarterSwingStrength - WindSpeed, HalfWindSpeed + 5, HalfWindSpeed - -5)
+    return BezierPoint1.CFrame * CFrame.new(QuarterSwingStrength - WindSpeed, HalfWindSpeed + 5, HalfWindSpeed -5)
 end
 
-function CFrameCalculator.CalculatePart3(HumanoidRootPart, SwingStrength, StartPart)
+function CFrameCalculator.CalculatePart3(BezierPoint1, SwingStrength)
+    local HalfSwingStrength = -SwingStrength / 2
+    local QuarterSwingStrength = -SwingStrength / 4
 
+    local HalfWindSpeed = -WindSpeed / 2
+    local QuarterWindSpeed = -WindSpeed / 4
+
+    return BezierPoint1.CFrame * CFrame.new(HalfWindSpeed - WindSpeed, HalfWindSpeed + 7, HalfWindSpeed -10)
 end
 
-function CFrameCalculator.CalculatePart4()
+function CFrameCalculator.CalculatePart4(BezierPoint1, SwingStrength)
+    local HalfSwingStrength = -SwingStrength / 2
+    local QuarterSwingStrength = -SwingStrength / 4
 
+    local HalfWindSpeed = -WindSpeed / 2
+    local QuarterWindSpeed = -WindSpeed / 4
+
+    return BezierPoint1.CFrame * CFrame.new(HalfWindSpeed - WindSpeed, 10, HalfWindSpeed - -15)
 end
 
 --EndModule
+
+
+--[[
+    Comments for Building Relevant Calculations
 return CFrameCalculator
 
---WillTakeThePartToTheLeft
---PartName.CFrame = PartName.CFrame * CFrame.new(0,0,1) -- Change this based on where you want to go, it's on an XYZ axis
+WillTakeThePartToTheLeft
+PartName.CFrame = PartName.CFrame * CFrame.new(0,0,1) -- Change this based on where you want to go, it's on an XYZ axis
 
---MidPart1.CFrame = StartPart.CFrame * CFrame.new((-SwingDistance / 4) -WindSpeedValue, 5 + (WindSpeedValue / 2), -5 - (WindSpeedValue / 2))
---MidPart2.CFrame = StartPart.CFrame * CFrame.new((-SwingDistance / 2) -WindSpeedValue, 7 + (WindSpeedValue / 2), -10 - (WindSpeedValue / 2))
---EndPart.CFrame = StartPart.CFrame * CFrame.new(-SwingDistance / 2, 10, -15 - (WindSpeedValue / 2))
+MidPart1.CFrame = StartPart.CFrame * CFrame.new((-SwingDistance / 4) -WindSpeedValue, 5 + (WindSpeedValue / 2), -5 - (WindSpeedValue / 2))
+MidPart2.CFrame = StartPart.CFrame * CFrame.new((-SwingDistance / 2) -WindSpeedValue, 7 + (WindSpeedValue / 2), -10 - (WindSpeedValue / 2))
+EndPart.CFrame = StartPart.CFrame * CFrame.new(-SwingDistance / 2, 10, -15 - (WindSpeedValue / 2))
+]]
